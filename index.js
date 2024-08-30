3fb8b361445b34dc9b9279255a90789d650ffd7d
AMBAR KUMAR <156634596+ambar1516@users.noreply.github.com>
2024-08-31 01:45:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+function toggleText1() {
+    var textElement = document.querySelector('.hidden-text1');
+    if (textElement.style.display === 'none' || textElement.style.display === '') {
+        textElement.style.display = 'block'; // Show the text
+    } else {
+        textElement.style.display = 'none'; // Hide the text
+    }
+}
+
+function toggleText2() {
+    var textElement = document.querySelector('.hidden-text2');
+    if (textElement.style.display === 'none' || textElement.style.display === '') {
+        textElement.style.display = 'block'; // Show the text
+    } else {
+        textElement.style.display = 'none'; // Hide the text
+    }
+}
+
+function toggleText3() {
+    var textElement = document.querySelector('.hidden-text3');
+    if (textElement.style.display === 'none' || textElement.style.display === '') {
+        textElement.style.display = 'block'; // Show the text
+    } else {
+        textElement.style.display = 'none'; // Hide the text
+    }
+}
+
+function toggleText4() {
+    var textElement = document.querySelector('.hidden-text4');
+    if (textElement.style.display === 'none' || textElement.style.display === '') {
+        textElement.style.display = 'block'; // Show the text
+    } else {
+        textElement.style.display = 'none'; // Hide the text
+    }
+}
+
+function toggleText5() {
+    var textElement = document.querySelector('.hidden-text5');
+    if (textElement.style.display === 'none' || textElement.style.display === '') {
+        textElement.style.display = 'block'; // Show the text
+    } else {
+        textElement.style.display = 'none'; // Hide the text
+    }
+}
+
+function toggleText6() {
+    var textElement = document.querySelector('.hidden-text6');
+    if (textElement.style.display === 'none' || textElement.style.display === '') {
+        textElement.style.display = 'block'; // Show the text
+    } else {
+        textElement.style.display = 'none'; // Hide the text
+    }
+}
+
+function toggleText7() {
+    var textElement = document.querySelector('.hidden-text7');
+    if (textElement.style.display === 'none' || textElement.style.display === '') {
+        textElement.style.display = 'block'; // Show the text
+    } else {
+        textElement.style.display = 'none'; // Hide the text
+    }
+}
+
+function toggleText8() {
+    var textElement = document.querySelector('.hidden-text8');
+    if (textElement.style.display === 'none' || textElement.style.display === '') {
+        textElement.style.display = 'block'; // Show the text
+    } else {
+        textElement.style.display = 'none'; // Hide the text
+    }
+}
+
+function toggleTexte() {
+    const button = document.querySelector('.icon-button');
+    div.classList.toggle('active'); // Toggle 'active' class to show/hide text
+}
